208ac7ec81be784698f36b57ef341543a8685fb4
ujjwalp2510 <ujjwalpandey2510@gmail.com>
2023-12-21 15:08:57 +0530

    Discussions error resolved

--- src/components/Meeting.js
+++ src/components/Meeting.js
@@ -14,7 +14,7 @@ passWord: location.state.livesession.passWord,
 
  userName: location.state.userData.name,
 userEmail: location.state.userData.email,
-leaveUrl: "https://cramix.onrender.com/dashboard",
+leaveUrl: "https://cramix.vercel.app/dashboard",
 };
 
 useEffect(() => {
